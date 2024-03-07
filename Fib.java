a7dcb2d510a5f6206a55c5db170f1cfa02c39650
Robert-ComputerScience <robert29578@gmail.com>
2024-03-07 09:52:29 -0500

    Done!!

--- Fib.java
+++ Fib.java
@@ -3,18 +3,18 @@ public class Fib
     // Fib modified results
 
     public static int fib(int n) {
-        if (n <= 0)
-            return 0;
-        else if (n == 1)
-            return 1;
+       if(n <= 1) {
+
+           return n;
+       }
         else
             return fib(n - 1) + fib(n - 2);
     }
-
+  // 6765 // Time complexity is O(2^n), it is provable through master theorem
     public static void main(String[] args)
     {
 
-        int result = fib(50); // You can replace with any number
+        int result = fib(20); // You can replace with any number
         System.out.println("Fib result: " + result);
 
 
